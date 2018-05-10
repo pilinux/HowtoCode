7300dffeddf46400f4ecb9d270667883a428a53c
Mahir Hasan <mahirhasan@live.com>
2018-05-10 22:02:33 +0200

    Create Statement.java

---
+++ Java/1.Intro/Statement.java
@@ -0,0 +1,30 @@
+package pilinux;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        //A complete statement:
+        //int a = 5;
+
+        a++;
+        //A complete statement:
+        //a++;
+
+        System.out.println("The value of a is: " + a);
+        //A complete statement:
+        //System.out.println("The value of a is: " + a);
+        
+        a++;    a--;
+        //A complete statement:
+        //a++;
+        //Another statement:
+        //a--;
+
+        System.out.println("The value of a is: "
+                + a);
+        //A complete statement:
+        //System.out.println("The value of a is: "
+        //                + a);
+    }
+}
