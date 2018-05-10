94801d8b0b88f590a163cb66cd6c71e01c88dae5
Mahir Hasan <mahirhasan@live.com>
2018-05-10 21:43:12 +0200

    Create Expression.java

---
+++ Java/1.Intro/Expression.java
@@ -0,0 +1,13 @@
+package pilinux;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        //Expression:
+        //a = 5
+        System.out.println("The value of a is: " + a);
+        //Expression:
+        //"The value of a is: " + a
+    }
+}
