fd9617c210cd4bbfee36a990d51ace4cd0aa57bd
Mahir Hasan <mahirhasan@live.com>
2018-05-11 08:37:37 +0200

    Create isLeapYear.java

---
+++ Java/Projects/LeapYear/isLeapYear.java
@@ -0,0 +1,10 @@
+    public static boolean isLeapYear(int year) {
+
+        if(year >= 1 && year <= 9999) {
+            if( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )
+                return true;
+        }
+        
+        return false;
+        
+    }
