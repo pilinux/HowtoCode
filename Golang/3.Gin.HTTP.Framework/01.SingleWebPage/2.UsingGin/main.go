53a42f42d2a14b7572846b57165e8124458fa883
Mahir <mahir.hasan@m.pilinux.me>
2019-01-19 05:52:07 +0100

    Gin Framework - single web page

---
+++ Golang/3.Gin.HTTP.Framework/01.SingleWebPage/2.UsingGin/main.go
@@ -0,0 +1,46 @@
+// main.go
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+
+	gin.SetMode(gin.ReleaseMode)
+
+	// Set the router as the default one provided by Gin
+	router = gin.Default()
+
+	// Process the templates at the start so that they don't have to be loaded
+	// from the disk again. This makes serving HTML pages very fast.
+	router.LoadHTMLGlob("template/*")
+
+	// Define the route for the index page and display the index.html template
+	// To start with, we'll use an in-line route handler. Later on, we'll create
+	// standalone functions that will be used as route handlers.
+	router.GET("/", func(c *gin.Context) {
+
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+
+	})
+
+	// Start serving the application (we're using port 3500)
+	router.Run(":3500")
+
+}
