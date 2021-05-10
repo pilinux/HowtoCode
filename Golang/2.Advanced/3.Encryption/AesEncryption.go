173b943f0de9f27ec1059c5cdc1e87aae9f48f7f
mahir <mahir.hasan@m.pilinux.me>
2021-05-10 11:51:42 +0200

    Golang: AES Encryption

---
+++ Golang/2.Advanced/3.Encryption/AesEncryption.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func main() {
+	// Create a slice of size 32 for generating a random 32-byte key for AES-256
+	// AES-128 requires a 16-byte key
+	// AES-192 requires a 24-byte key
+	bytes := make([]byte, 32)                   // [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+	if _, err := rand.Read(bytes); err != nil { // random 32-byte key
+		panic(err.Error())
+	}
+
+	key := hex.EncodeToString(bytes) //encode key in bytes to string and keep as secret, put in a vault
+	fmt.Printf("key to encrypt/decrypt : %s\n", key)
+
+	encrypted := encrypt("Hello world!", key)
+	fmt.Printf("encrypted : %s\n", encrypted)
+
+	decrypted := decrypt(encrypted, key)
+	fmt.Printf("decrypted : %s\n", decrypted)
+}
+
+// encrypt ...
+func encrypt(stringToEncrypt string, keyString string) (encryptedString string) {
+
+	// Decode the key from string to bytes
+	key, _ := hex.DecodeString(keyString)
+
+	plaintext := []byte(stringToEncrypt)
+
+	//Create a new Cipher Block from the key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Create a new GCM
+	// https://en.wikipedia.org/wiki/Galois/Counter_Mode
+	// https://golang.org/pkg/crypto/cipher/#NewGCM
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Create a nonce. Nonce should be from GCM
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	// Encrypt the data using aesGCM.Seal
+	// Add the nonce as a prefix to the encrypted data. The first nonce argument in Seal is the prefix.
+	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
+	return fmt.Sprintf("%x", ciphertext)
+}
+
+// decrypt ...
+func decrypt(encryptedString string, keyString string) (decryptedString string) {
+
+	key, _ := hex.DecodeString(keyString)
+	enc, _ := hex.DecodeString(encryptedString)
+
+	//Create a new Cipher Block from the key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Create a new GCM
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Get the nonce size
+	nonceSize := aesGCM.NonceSize()
+
+	// Extract the nonce from the prefix of the encrypted data
+	nonce, ciphertext := enc[:nonceSize], enc[nonceSize:]
+
+	// Decrypt the data
+	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return fmt.Sprintf("%s", plaintext)
+}
