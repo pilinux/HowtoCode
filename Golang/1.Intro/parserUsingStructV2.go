2fb3af79398f511a2ebe05e98b99291676128751
pilinux <mahir.hasan@m.pilinux.me>
2022-07-19 12:05:28 +0200

    Golang: parsing log using struct v2

---
+++ Golang/1.Intro/parserUsingStructV2.go
@@ -0,0 +1,93 @@
+// continuation of parserUsingStruct.go
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type result struct {
+	domain string
+	visits int
+}
+
+type parser struct {
+	sum     map[string]result // total visits per domain
+	domains []string          // slice to keep domain names in a sequence
+	total   int               // total visits to all domain
+	lines   int               // number of parsed line
+}
+
+func main() {
+	p := parser{
+		sum: make(map[string]result),
+	}
+
+	in := bufio.NewScanner(os.Stdin)
+
+	for in.Scan() {
+		p.lines++
+
+		// split the string s around each instance of one or more consecutive white space
+		fields := strings.Fields(in.Text())
+		if len(fields) != 2 {
+			fmt.Printf("wrong input: %v at line #%d\n", fields, p.lines)
+			fmt.Println("existing...")
+			return
+		}
+
+		domain := fields[0]
+		visits, err := strconv.Atoi(fields[1])
+		if visits < 0 || err != nil {
+			fmt.Printf("wrong input: %v at line #%d\n", fields[1], p.lines)
+			fmt.Println("existing...")
+			return
+		}
+
+		if _, ok := p.sum[domain]; !ok {
+			p.domains = append(p.domains, domain)
+		}
+
+		// a value can be assigned to addressable elements
+		// map elements are not addressable
+		// hence, this won't work
+		// p.sum[domain].visits += visits
+		p.sum[domain] = result{
+			domain: domain,
+			visits: visits + p.sum[domain].visits, // current visits + old visits
+		}
+
+		p.total += visits
+	}
+
+	// after "Domain", add spaces
+	// before "Visits", add spaces
+	fmt.Printf("%-20s %10s\n", "Domain", "Visits")
+	fmt.Println(strings.Repeat("-", 35))
+
+	sort.Strings(p.domains)
+	for _, domain := range p.domains {
+		parsed := p.sum[domain]
+		fmt.Printf("%-20s %10d\n", domain, parsed.visits)
+	}
+
+	fmt.Println(strings.Repeat("_", 35))
+	fmt.Printf("%-20s %10d\n", "TOTAL", p.total)
+
+	if err := in.Err(); err != nil {
+		fmt.Println("> Err: ", err)
+		return
+	}
+}
+
+/*
+./parserUsingStructV2 < parserUsingStruct.log1.txt
+./parserUsingStructV2 < parserUsingStruct.log2.txt
+./parserUsingStructV2 < parserUsingStruct.log3.txt
+./parserUsingStructV2 < parserUsingStruct.log4.txt
+*/
